8c0e69c5a14ea5426ff317898054d1e83775a064
Julien - SoEasY <julien@rouillier.Fr>
2021-02-17 22:03:43 +0100

    Finished ARMada solver

--- Reverse/ARMada/SOLVER_ARMada.py
+++ Reverse/ARMada/SOLVER_ARMada.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import base64
+from pwn import *
 
 STANDARD_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 CUSTOM_ALPHABET = 'CTwGhcJj+nKSqARsQ27omX0Iley91ufDbPxVY4ar5UgMNt/L3BvzkFiHZW6dOp8E'
@@ -8,5 +11,22 @@ def custom64Decode(input):
     newStr =  input.translate(DECODE_TRANS) + '='
     return base64.b64decode(newStr)
 
-stringg = str(input("Entre un truc à décoder : "))
-print(custom64Decode(stringg))
+def main():
+    r = remote("127.0.0.1", 3000)
+
+    rep = ""
+    while "Hero{" not in rep:
+        try:
+            r.recvuntil("New cipher : ", drop=True)
+        except:
+            print("\nFLAG : " + str(r.recvline())[2:][:-3])
+            break
+        cipher = str(r.recvline().replace(b'\n', b''))[2:][:-1]
+        log.success("CIPHER : " + cipher)
+        answer = custom64Decode(str(cipher))
+        log.success(b"ANSWER : " + answer)
+        r.sendline(answer.strip())
+        rep = str(r.recvline())
+
+if __name__ == "__main__":
+    main()
