ef6831777963df2f01979943147d4fabb1d47b78
xanhacks <xanhacks@protonmail.com>
2021-04-04 17:06:01 +0200

    remove infra files

--- System/Assistant/start.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-cron -f&
-/usr/sbin/sshd -D
--- System/PrivesCorp_3_HelpMe/passManager.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main() {
-	std::cout << "[*] bob : password123" << std::endl;
-	std::cout << "[*] dylan : I_h4t3_my_j0b" << std::endl;
-	std::cout << "[*] rh : $3cur3_l0l" << std::endl;
-}
