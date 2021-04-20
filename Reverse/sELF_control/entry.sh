9e7620dc7f254dbd414dc6b9678a3877a032140c
xanhacks <xanhacks@protonmail.com>
2021-04-20 21:37:06 +0200

    remove su

--- Reverse/sELF_control/entry.sh
+++ Reverse/sELF_control/entry.sh
@@ -2,5 +2,5 @@
 
 while :
 do
-	su -c "exec socat TCP-LISTEN:$LISTEN_PORT,reuseaddr,fork EXEC:'/usr/src/sELF_control,stderr'" - heroctf;
+	exec socat TCP-LISTEN:$LISTEN_PORT,reuseaddr,fork EXEC:'/usr/src/sELF_control,stderr'
 done
