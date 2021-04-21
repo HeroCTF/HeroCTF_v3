9ce733c439a80df4e354d46d16f5f3c077c30c2d
xanhacks <xanhacks@protonmail.com>
2021-04-21 20:31:53 +0200

    add debug mode

--- Web/PwnQL_1/src/config.php
+++ Web/PwnQL_1/src/config.php
@@ -2,6 +2,12 @@
 
 define("FLAG", getenv("PWNQL_1_FLAG"));
 
+if (getenv('DEBUG') === 'true') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
 $mysq_host = getenv('MYSQL_HOST');
 $mysql_user = getenv("MYSQL_USER");
 $mysql_password = getenv("MYSQL_PASSWORD");
