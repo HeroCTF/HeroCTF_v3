3872e7c934c622a6212f2ec3c208afd1ec45cf79
xanhacks <xanhacks@protonmail.com>
2021-04-21 20:37:09 +0200

    add pdo error

--- Web/PwnQL_1/src/config.php
+++ Web/PwnQL_1/src/config.php
@@ -2,7 +2,11 @@
 
 define("FLAG", getenv("PWNQL_1_FLAG"));
 
-if (getenv('DEBUG') === 'true') {
+function debugMode() {
+    return getenv('DEBUG') === 'true';
+}
+
+if (debugMode()) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -14,12 +18,19 @@ $mysql_password = getenv("MYSQL_PASSWORD");
 $mysql_database = getenv("MYSQL_DATABASE");
 
 try {
-    $db = new PDO(
+    $pdo = new PDO(
         "mysql:host=${mysq_host};dbname=${mysql_database};charset=utf8",
         $mysql_user,
         $mysql_password
     );
+    if (debugMode()) {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
 } catch (PDOException $e) {
     echo("Error : Failed to connect to the database, please wait !");
+    if (debugMode()) {
+        var_dump($e->getMessage());
+    }
     die();
 }
