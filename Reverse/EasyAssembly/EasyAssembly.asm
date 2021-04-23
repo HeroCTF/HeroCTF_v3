338d1898b08b1e7ab5ade7a584e088ac4c55a689
SoEasY <34216946+0xSoEasY@users.noreply.github.com>
2021-04-23 10:47:21 +0200

    Update EasyAssembly.asm

--- Reverse/EasyAssembly/EasyAssembly.asm
+++ Reverse/EasyAssembly/EasyAssembly.asm
@@ -31,7 +31,7 @@ getInput:
         subq    $32, %rsp       #,
 # EasyAssembly.c:7: int getInput(void){
         movq    %fs:40, %rax    # MEM[(<address-space-1> long unsigned int *)40B], tmp88
-        movq    %rax, -8(%rbp)  # tmp88, D.2853
+        movq    %rax, -8(%rbp)  # tmp88, D.2854
         xorl    %eax, %eax      # tmp88
 # EasyAssembly.c:10:    printf("Hey ! Have you got a password for me ? ");
         leaq    .LC0(%rip), %rdi        #,
@@ -48,7 +48,7 @@ getInput:
         movq    %rax, %rdi      # tmp86,
         call    atoi@PLT        #
 # EasyAssembly.c:13: }
-        movq    -8(%rbp), %rcx  # D.2853, tmp89
+        movq    -8(%rbp), %rcx  # D.2854, tmp89
         xorq    %fs:40, %rcx    # MEM[(<address-space-1> long unsigned int *)40B], tmp89
         je      .L3     #,
         call    __stack_chk_fail@PLT    #
@@ -62,7 +62,7 @@ getInput:
         .section        .rodata
         .align 8
 .LC1:
-        .string "Well done ! You can validate with the flag Hero{%d}\n"
+        .string "Well done ! You can validate with the flag Hero{%d:%d}\n"
         .align 8
 .LC2:
         .string "Argh... Try harder buddy you can do it !"
@@ -79,37 +79,40 @@ main:
         movq    %rsp, %rbp      #,
         .cfi_def_cfa_register 6
         subq    $16, %rsp       #,
-# EasyAssembly.c:16:    int input = getInput();
+# EasyAssembly.c:17:    int input = getInput();
         call    getInput        #
-        movl    %eax, -4(%rbp)  # tmp85, input
-# EasyAssembly.c:18:    input = input >> 2;
-        sarl    $2, -4(%rbp)    #, input
-# EasyAssembly.c:20:    if(input == 1337404)
-        cmpl    $1337404, -4(%rbp)      #, input
+        movl    %eax, -8(%rbp)  # tmp85, input
+# EasyAssembly.c:19:    modified = input >> 2;
+        movl    -8(%rbp), %eax  # input, tmp89
+        sarl    $2, %eax        #, tmp88
+        movl    %eax, -4(%rbp)  # tmp88, modified
+# EasyAssembly.c:21:    if(modified == 1337404)
+        cmpl    $1337404, -4(%rbp)      #, modified
         jne     .L5     #,
-# EasyAssembly.c:21:            isGood = 0;
+# EasyAssembly.c:22:            isGood = 0;
         movl    $0, isGood(%rip)        #, isGood
 .L5:
-# EasyAssembly.c:23:    if(!isGood)
+# EasyAssembly.c:24:    if(!isGood)
         movl    isGood(%rip), %eax      # isGood, isGood.1_1
-# EasyAssembly.c:23:    if(!isGood)
+# EasyAssembly.c:24:    if(!isGood)
         testl   %eax, %eax      # isGood.1_1
         jne     .L6     #,
-# EasyAssembly.c:24:            printf("Well done ! You can validate with the flag Hero{%d}\n", input);
-        movl    -4(%rbp), %eax  # input, tmp86
-        movl    %eax, %esi      # tmp86,
+# EasyAssembly.c:25:            printf("Well done ! You can validate with the flag Hero{%d:%d}\n", input, modified);
+        movl    -4(%rbp), %edx  # modified, tmp90
+        movl    -8(%rbp), %eax  # input, tmp91
+        movl    %eax, %esi      # tmp91,
         leaq    .LC1(%rip), %rdi        #,
         movl    $0, %eax        #,
         call    printf@PLT      #
         jmp     .L7     #
 .L6:
-# EasyAssembly.c:27:            puts("Argh... Try harder buddy you can do it !");
+# EasyAssembly.c:28:            puts("Argh... Try harder buddy you can do it !");
         leaq    .LC2(%rip), %rdi        #,
         call    puts@PLT        #
 .L7:
-# EasyAssembly.c:29:    return EXIT_SUCCESS;
+# EasyAssembly.c:30:    return EXIT_SUCCESS;
         movl    $0, %eax        #, _11
-# EasyAssembly.c:30: }
+# EasyAssembly.c:31: }
         leave
         .cfi_def_cfa 7, 8
         ret
