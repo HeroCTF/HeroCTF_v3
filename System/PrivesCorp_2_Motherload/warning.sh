90c8b4985d98f91681b8bf4d29730529236f565e
Léo Desmonts <leo951206@gmail.com>
2021-04-25 01:07:10 +0200

    Update warning.sh

--- System/PrivesCorp_2_Motherload/warning.sh
+++ System/PrivesCorp_2_Motherload/warning.sh
@@ -4,7 +4,7 @@ if [ "$1" = "-ld" ] && [ "$2" = "/home/bob/.financial.txt" ]
 then
     echo "-rwx------ 1 accountant accountant   113 avril 21 21:44 /home/bob/.financial.txt"
 else
-    echo "This is a shared instance. You can right in /home/bob/ but it will deleted every half second"
+    echo "This is a shared instance. You can write in /home/bob/ but it will deleted every half second"
     echo "/tmp will be cleared every minute aswell"
     echo "-rwsr-xr-x 1 root root   113 avril 21 21:44 /home/bob/exportSafePerms"
 fi
