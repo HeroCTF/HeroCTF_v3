b1d5cd734ccd9fe1e9b25cc5012c3369a1991ae2
Léo Desmonts <leo951206@gmail.com>
2021-04-24 13:51:35 +0200

    Update clear.sh

--- System/PrivesCorp_1_Probation/clear.sh
+++ System/PrivesCorp_1_Probation/clear.sh
@@ -4,4 +4,5 @@ rm -rf /home/bob/* /tmp/*
 cp /root/locked.txt /home/bob/locked.txt
 cp /roo/note.txt /home/bob/note.txt
 chmod 500 /home/bob/locked.txt
+chown bob:bob /home/bob/note.txt
 chmod 544 /home/bob/note.txt
