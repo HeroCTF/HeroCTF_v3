c0df5a1694de5049d8fb85e3fb91e7e45bdf44d5
Léo Desmonts <leo951206@gmail.com>
2021-04-24 13:57:08 +0200

    Update warning.sh

--- System/PrivesCorp_1_Probation/warning.sh
+++ System/PrivesCorp_1_Probation/warning.sh
@@ -2,4 +2,4 @@
 echo "This is a shared instance. The /home/bob and /tmp directories will be cleared every 3 minutes"
 echo "The locked.txt file cannot be made worldreadable, but there are other easy ways to get the flag !"
 echo ""
-echo "echo "-rwxr--r-- 1 bob bob   113 avril 21 21:44 /home/bob/note.txt"
+echo "echo '-rwxr--r-- 1 bob bob   113 avril 21 21:44 /home/bob/note.txt'"
